edb0df096624a2b24ffec2fbed7d73a4bd34507e
glejeune <gregoire.lejeune@free.fr>
2016-06-09 15:47:42 +0200

    Add mix.exs

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Lager.Json.Formatter.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :lager_json_formatter,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
+  end
+
+  def application do
+    [
+       applications: [],
+       env: []
+    ]
+  end
+
+  defp deps do
+    [
+      {:lager, "~> 3.2"}    
+    ]
+  end
+end
